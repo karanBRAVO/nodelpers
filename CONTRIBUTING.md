be404389f4da5a98f3a624e001b8d9bdc674730a
D@reK@r@n18 <xpresskaran98@gmail.com>
2025-01-09 18:42:31 +0530

    Deploy website - based on f7daf72f6e0d1fdeb3e064d9b257023b8b5e9445

--- CONTRIBUTING.md
+++
@@ -1,3 +0,0 @@
-# Contributing Guidelines
-
-_Pull requests, bug reports, and all other forms of contribution are welcomed and highly encouraged!_
--- docs/src/lib/types/home.ts
+++
@@ -1,7 +0,0 @@
-import { type ReactNode } from "react";
-
-export type FeatureItem = {
-  title: string;
-  Svg: React.ComponentType<React.ComponentProps<"svg">>;
-  description: ReactNode;
-};
