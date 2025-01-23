fa607301b2a4fe6b201cc86cd7247b6777e3f40f
D@reK@r@n18 <xpresskaran98@gmail.com>
2025-01-24 00:44:21 +0530

    format the files using prettier

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@
 export * from "./utils/number.utils";
 export * from "./utils/date.utils";
 export * from "./utils/string.utils";
+export * from "./utils/time.utils";
+export * from "./utils/fs.utils";
 
 // lib/
 export * from "./lib/index";
--- src/lib/errors/index.ts
+++ src/lib/errors/index.ts
@@ -1,3 +1,4 @@
 export * from "./BaseError";
 export * from "./InvalidFormatError";
 export * from "./InvalidTimeValuesError";
+export * from "./PathError";
