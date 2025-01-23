ea2f849a8d6df4be6b206de8c8e5be218e352309
D@reK@r@n18 <xpresskaran98@gmail.com>
2025-01-24 00:54:27 +0530

    add the husky dev dependency

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,7 +4,10 @@ import tseslint from "typescript-eslint";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { files: ["**/*.{js,mjs,cjs,ts}"], ignores: ["node_modules/", "dist/"] },
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    ignores: ["node_modules/", "dist/", ".husky/"],
+  },
   { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
