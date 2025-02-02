308d0e21953df3050cc71515cfc83421b225e370
Sansh2356 <sansh2356@gmail.com>
2025-02-02 20:48:47 +0530

    updating the linking

--- docs/src/components/HomepageFeatures/index.tsx
+++ docs/src/components/HomepageFeatures/index.tsx
@@ -43,15 +43,17 @@ const FeatureList: FeatureItem[] = [
 
 const Feature = ({ title, Svg, description }: FeatureItem) => {
   return (
-    <div className={clsx("col col--4")}>
-      <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
+    <>
+      <div className={clsx("col col--4")}>
+        <div className="text--center">
+          <Svg className={styles.featureSvg} role="img" />
+        </div>
+        <div className="text--center padding-horiz--md">
+          <Heading as="h3">{title}</Heading>
+          <p>{description}</p>
+        </div>
       </div>
-      <div className="text--center padding-horiz--md">
-        <Heading as="h3">{title}</Heading>
-        <p>{description}</p>
-      </div>
-    </div>
+    </>
   );
 };
 
